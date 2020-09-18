2a809330078399b352029e7f26cb462cc8814f5d
mrsilvino <silvinomartins991@gmail.com>
2020-09-18 16:44:30 +0100

    added ErrorBoundry functionality

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import './Estilos.css';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 
 class App extends Component {
     
@@ -46,7 +47,9 @@ class App extends Component {
                         <SearchBox searchChange={this.onSearchChange} />
                     </header>
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </main>
             )
