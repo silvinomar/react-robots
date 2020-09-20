b3252ad314af26ca66589b2b1556511854d4adbc
mrsilvino <silvinomartins991@gmail.com>
2020-09-20 15:28:34 +0100

    Updates

---
+++ precache-manifest.dbbe5f4eff94ae9b649527e5c5fd44bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016883d345e75395db8b43384bad26b0",
+    "url": "/react-robots/index.html"
+  },
+  {
+    "revision": "fb5cae0ba8c2e435462a",
+    "url": "/react-robots/static/css/main.5a69545f.chunk.css"
+  },
+  {
+    "revision": "be5ce207228d157402e0",
+    "url": "/react-robots/static/js/2.a9c3db33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robots/static/js/2.a9c3db33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5cae0ba8c2e435462a",
+    "url": "/react-robots/static/js/main.29337973.chunk.js"
+  },
+  {
+    "revision": "07d4ad8f562e2ff4033a",
+    "url": "/react-robots/static/js/runtime-main.4274f8a0.js"
+  }
+]);
